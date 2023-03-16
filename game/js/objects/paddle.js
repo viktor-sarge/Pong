373f03efc82e93a1f9abaf3bcfac3d12af498f82
Viktor Sarge <viktor.sarge@regionhalland.se>
2023-03-16 21:40:31 +0200

    refactor(paddle): 🔥 Remove unused collision checking from paddle class
    Raderat oanvänd kollisionscheck från spelarklassen när motorn nu används

--- game/js/objects/paddle.js
+++ game/js/objects/paddle.js
@@ -35,23 +35,6 @@ export default class Paddle extends Rectangle {
       this.drawBoostReload(this.ctx);
     }
 
-    intersects(ball) {
-      // Check if the ball is within the left and right edges of the paddle
-      let ballX = ball.x + ball.radius;
-      if (ballX < this.x || ballX > this.x + this.width) {
-          return false;
-      }
-
-      // Check if the ball is within the top and bottom edges of the paddle
-      let ballY = ball.y + ball.radius;
-      if (ballY < this.y || ballY > this.y + this.height) {
-          return false;
-      }
-
-      // If the ball is within both the horizontal and vertical edges of the paddle, it intersects with the paddle
-      return true;
-    }
-
     shrink() {
       if(this.height > this.minHeight) { 
         this.height = this.height - this.rescaleStep;
